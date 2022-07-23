79a4f0392e6c75c0dd82f69d6833946577d0f848
sweak <sweakpolska@gmail.com>
2022-07-23 14:08:10 +0200

    Added support for different screen sizes + minor improvements in WelcomeScreen

--- app/src/main/java/com/example/egzamindyplomowy/presentation/introduction/WelcomeScreen.kt
+++ app/src/main/java/com/example/egzamindyplomowy/presentation/introduction/WelcomeScreen.kt
@@ -2,23 +2,114 @@ package com.example.egzamindyplomowy.presentation.introduction
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.egzamindyplomowy.R
+import com.example.egzamindyplomowy.presentation.WindowInfo
+import com.example.egzamindyplomowy.presentation.rememberWindowInfo
 import com.example.egzamindyplomowy.presentation.ui.theme.space
 
 @Composable
 fun WelcomeScreen() {
-    Surface(
-        color = Color.Transparent,
+    val windowInfo = rememberWindowInfo()
+
+    if (windowInfo.screenWidthInfo is WindowInfo.WindowType.Compact) {
+        CompactWelcomeScreen()
+    } else {
+        MediumOrExpandedWelcomeScreen()
+    }
+}
+
+@Composable
+fun CompactWelcomeScreen() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                Brush.verticalGradient(
+                    listOf(
+                        MaterialTheme.colors.primary,
+                        MaterialTheme.colors.primaryVariant
+                    )
+                )
+            )
+            .verticalScroll(rememberScrollState())
+    ) {
+        Spacer(modifier = Modifier.height(MaterialTheme.space.medium))
+
+        Icon(
+            painter = painterResource(id = R.drawable.ic_application_icon),
+            contentDescription = "Application icon - university graduation cap",
+            tint = MaterialTheme.colors.onPrimary,
+            modifier = Modifier
+                .size(96.dp)
+        )
+
+        Spacer(modifier = Modifier.height(MaterialTheme.space.medium))
+
+        Text(
+            text = stringResource(R.string.app_name),
+            style = MaterialTheme.typography.h1,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(
+                bottom = MaterialTheme.space.extraLarge,
+                start = MaterialTheme.space.large,
+                end = MaterialTheme.space.large
+            )
+        )
+
+        Text(
+            text = stringResource(R.string.welcome_to_exam),
+            style = MaterialTheme.typography.h2,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(
+                bottom = MaterialTheme.space.medium,
+                start = MaterialTheme.space.large,
+                end = MaterialTheme.space.large
+            )
+        )
+
+        Text(
+            text = stringResource(R.string.tell_me_who_you_are),
+            style = MaterialTheme.typography.h2,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(
+                bottom = MaterialTheme.space.large,
+                start = MaterialTheme.space.large,
+                end = MaterialTheme.space.large
+            )
+        )
+
+        ExamRoleChoiceButton(
+            text = stringResource(R.string.student_instrumental),
+            onClick = {/*TODO*/ }
+        )
+
+        Spacer(modifier = Modifier.height(MaterialTheme.space.large))
+
+        ExamRoleChoiceButton(
+            text = stringResource(R.string.examiner_instrumental),
+            onClick = {/*TODO*/ }
+        )
+
+        Spacer(modifier = Modifier.height(MaterialTheme.space.large))
+    }
+}
+
+@Composable
+fun MediumOrExpandedWelcomeScreen() {
+    Row(
         modifier = Modifier
             .fillMaxSize()
             .background(
@@ -31,11 +122,11 @@ fun WelcomeScreen() {
             )
     ) {
         Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
             modifier = Modifier
-                .fillMaxSize()
-                .padding(horizontal = MaterialTheme.space.large)
+                .fillMaxHeight()
+                .weight(1f),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Icon(
                 painter = painterResource(id = R.drawable.ic_application_icon),
@@ -43,28 +134,50 @@ fun WelcomeScreen() {
                 tint = MaterialTheme.colors.onPrimary,
                 modifier = Modifier
                     .size(96.dp)
-                    .padding(bottom = MaterialTheme.space.medium)
             )
+
+            Spacer(modifier = Modifier.height(MaterialTheme.space.medium))
 
             Text(
                 text = stringResource(R.string.app_name),
                 style = MaterialTheme.typography.h1,
                 textAlign = TextAlign.Center,
-                modifier = Modifier.padding(bottom = MaterialTheme.space.extraLarge)
+                modifier = Modifier.padding(
+                    bottom = MaterialTheme.space.medium,
+                    start = MaterialTheme.space.large,
+                    end = MaterialTheme.space.large
+                )
             )
 
             Text(
                 text = stringResource(R.string.welcome_to_exam),
                 style = MaterialTheme.typography.h2,
                 textAlign = TextAlign.Center,
-                modifier = Modifier.padding(bottom = MaterialTheme.space.medium)
+                modifier = Modifier.padding(
+                    bottom = MaterialTheme.space.medium,
+                    start = MaterialTheme.space.large,
+                    end = MaterialTheme.space.large
+                )
             )
+        }
 
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .weight(1f),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Text(
                 text = stringResource(R.string.tell_me_who_you_are),
                 style = MaterialTheme.typography.h2,
                 textAlign = TextAlign.Center,
-                modifier = Modifier.padding(bottom = MaterialTheme.space.extraLarge)
+                modifier = Modifier.padding(
+                    top = MaterialTheme.space.medium,
+                    bottom = MaterialTheme.space.large,
+                    start = MaterialTheme.space.large,
+                    end = MaterialTheme.space.large
+                )
             )
 
             ExamRoleChoiceButton(
@@ -78,6 +191,8 @@ fun WelcomeScreen() {
                 text = stringResource(R.string.examiner_instrumental),
                 onClick = {/*TODO*/ }
             )
+
+            Spacer(modifier = Modifier.height(MaterialTheme.space.medium))
         }
     }
 }
@@ -96,6 +211,10 @@ fun ExamRoleChoiceButton(
         modifier = Modifier
             .fillMaxWidth()
             .height(52.dp)
+            .padding(
+                start = MaterialTheme.space.large,
+                end = MaterialTheme.space.large
+            )
     ) {
         Text(
             text = text,
